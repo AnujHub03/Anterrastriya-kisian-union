714d1999d36a3d55dff07ba1f36596ffdc8d4bc0
Anuj Hooda <hoodaanuj03@gmail.com>
2026-09-06 13:45:06 +0530

    Feat :fixing routes in server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/Authroutes.js";
 import galleryRoutes from "./routes/galleryRoutes.js";
 import sankalpRoutes from "./routes/Sankalproutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
