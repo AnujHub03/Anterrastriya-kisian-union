cbbec2e6bb31d3fd88009754b9e010ed82888372
Ankushdhull9908 <ankushdhull9908@gmail.com>
2026-09-06 17:43:27 +0530

    fixed api

--- client/src/Pages/AuthCont.jsx
+++ client/src/Pages/AuthCont.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from 'react'
 
 const AuthContext = createContext(null)
 
-const API_BASE = window.location.hostname == "localhost"?'http://localhost:5000': 'https://anterrastriya-kisian-union.onrender.com'
+const API_BASE = window.location.hostname == "localhost"?'http://localhost:5000/api/auth': 'https://anterrastriya-kisian-union.onrender.com/api/auth'
 
 const safeParseJSON = async (res) => {
   const text = await res.text()
